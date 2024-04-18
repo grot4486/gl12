1271938c5b349cbd91bfff6fef550b40be028e3d
grot4486 <115872950+grot4486@users.noreply.github.com>
2024-04-18 04:13:39 +0000

    cat.c

--- cat.c
+++ cat.c
@@ -3,25 +3,35 @@
 
 int count = 100000;
 
-void* change()
-{
+
+pthread_mutex_t change_lock = PTHREAD_MUTEX_INITIALIZER;
+
+void *change(void *arg) {
     for (int i = 0; i < 10000; i++) {
-        count = count -1;
+        pthread_mutex_lock(&change_lock);
+        count++;
+        pthread_mutex_unlock(&change_lock);
     }
     return NULL;
 }
 
-int main()
-{
+
+int main() {
     pthread_t threads[10];
+    
     printf("Счетчик равен %d\n", count);
+
     for (int i = 0; i < 10; i++) {
         pthread_create(&threads[i], NULL, change, NULL);
     }
+
     void *result;
+
     for (int i = 0; i < 10; i++) {
         pthread_join(threads[i], &result);
     }
+
     printf("Счетчик равен %d\n", count);
+    
     return 0;
 }
